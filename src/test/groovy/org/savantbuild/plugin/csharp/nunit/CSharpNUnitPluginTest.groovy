815102ab930c12a0c88a65c5c08900c428ece898
Brian Pontarelli <brian@inversoft.com>
2016-09-20 15:26:44 -0600

    Renamed packages

--- src/test/groovy/org/savantbuild/plugin/csharp/nunit/CSharpNUnitPluginTest.groovy
+++ src/test/groovy/org/savantbuild/plugin/csharp/nunit/CSharpNUnitPluginTest.groovy
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  */
-package org.savantbuild.plugin.csharp.testng
+package org.savantbuild.plugin.csharp.nunit
 
 import java.nio.file.Files
 import java.nio.file.Path
